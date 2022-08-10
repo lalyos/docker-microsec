0d537d3d2f7532b2b16454325e77d9f8fb98e650
lalyos <lalyos@yahoo.com>
2022-08-10 10:16:55 +0200

    customer table addded

--- sql/init.sql
+++ sql/init.sql
@@ -2,3 +2,14 @@
  insert into vip values ('vladimir', 66);
  insert into vip values ('victor', 62);
  insert into vip values ('angela', 55);
+
+
+CREATE TABLE customer (
+    id bigint NOT NULL,
+    first_name character varying(255),
+    last_name character varying(255)
+);
+
+INSERT INTO "customer" VALUES ('1', 'Vladimir', 'Vladimirovics');
+INSERT INTO "customer" VALUES ('2', 'Victo', 'Netuddki');
+INSERT INTO "customer" VALUES ('3', 'Angela', 'German');
